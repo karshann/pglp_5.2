7379aa030119dd86f2ddd31699ad7be0a1fd9a80
karshann <karshan951@gmail.com>
2020-05-22 18:39:40 +0200

    ajou dao compositeforme

--- src/main/java/org/example/CompositePersonnelDao.java
+++ src/main/java/org/example/CompositePersonnelDao.java
@@ -1,14 +1,15 @@
 package org.example;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CompositePersonnelDao extends Dao<CompositePersonnel>{
     @Override
     public CompositePersonnel create(CompositePersonnel obj) {
        this.connect();
-       for (Composite composite : obj.enfantComposite) {
-           try (PreparedStatement insert = this.connect.prepareStatement("INSERT INTO CompositePersonnel(Nom, Nomobj , type) values(?, ?, ?, ?)");) {
+        try (PreparedStatement insert = this.connect.prepareStatement("INSERT INTO COMPOSITEPERSONNEL(Nom, Nomobj , type) values(?, ?, ?)");) {
+        for (Composite composite : obj.enfantComposite) {
                insert.setString(1, obj.return_name());
                insert.setString(2, composite.return_name());
                if (composite instanceof Personnel) {
@@ -16,26 +17,73 @@ public class CompositePersonnelDao extends Dao<CompositePersonnel>{
                    personnelDAO.create((Personnel) composite);
                    insert.setInt(3, 1);
                } else {
-                   CompositePersonnelDAO compositePersonnelDAO = new CompositePersonnelDAO();
+                   CompositePersonnelDao compositePersonnelDAO = new CompositePersonnelDao();
                    compositePersonnelDAO.create((CompositePersonnel) composite);
                    insert.setInt(3, 0);
                }
                insert.executeUpdate();
-           } catch (SQLException throwables) {
-               throwables.printStackTrace();
-
            }
        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+
+        }
        return null;
     }
 
     @Override
     public CompositePersonnel find(String id) {
-        return null;
+        CompositePersonnel compositePersonnel = null;
+        this.connect();
+        try (PreparedStatement select =
+                     this.connect.prepareStatement("SELECT * FROM COMPOSITEPERSONNEL CP WHERE CP.Nom = ?")) {
+            select.setString(1, id);
+            compositePersonnel = new CompositePersonnel(id);
+            try {
+                ResultSet res = select.executeQuery();
+                while (res.next()) {
+                    if (res.getInt("type") == 0) {
+                        CompositePersonnelDao compositePersonnelDao = new CompositePersonnelDao();
+                        compositePersonnel.add(compositePersonnelDao.find(res.getString("Nomobj")));
+                    } else {
+                        PersonnelDAO personnelDAO = new PersonnelDAO();
+                        compositePersonnel.add(personnelDAO.find(res.getString("Nomobj")));
+                        System.out.println("reussi");
+                    }
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return compositePersonnel;
     }
 
     @Override
     public void delete(String s) {
-
+        this.connect();
+        try (PreparedStatement select =
+                     this.connect.prepareStatement("SELECT * FROM COMPOSITEPERSONNEL CP WHERE CP.Nom = ?")) {
+            select.setString(1, s);
+            try {
+                ResultSet res = select.executeQuery();
+                while (res.next()) {
+                    if (res.getInt("type") == 0) {
+                        CompositePersonnelDao compositePersonnelDao = new CompositePersonnelDao();
+                        compositePersonnelDao.delete("Nomobj");
+                    } else {
+                        PersonnelDAO personnelDAO = new PersonnelDAO();
+                        personnelDAO.delete(s);
+                    }
+                }
+                PreparedStatement delete = this.connect.prepareStatement("DELETE FROM COMPOSITEPERSONNEL CP WHERE CP.Nom = ?");
+                delete.setString(1,s);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
