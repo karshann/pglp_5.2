3c913c0b83b6baf38ad3658fae7212f38550e8b6
karshann <karshan951@gmail.com>
2020-05-22 18:42:11 +0200

    ajout des test de CompositeForme

--- src/test/java/org/example/CompositePersonnelDaoTest.java
+++ src/test/java/org/example/CompositePersonnelDaoTest.java
@@ -10,7 +10,7 @@ public class CompositePersonnelDaoTest {
 
     @Test
     public void create() {
-        CompositePersonnelDAO compositePersonnelDAO=new CompositePersonnelDAO();
+        CompositePersonnelDao compositePersonnelDAO=new CompositePersonnelDao();
         Personnel P=new Personnel.Builder("Jean","Paul").telephone("02589445272").naissance(LocalDateTime.of(1997,11,18,21,05)).fonction("PDG").build();
         CompositePersonnel compositePersonnel=new CompositePersonnel("groupe");
         compositePersonnel.add(P);
@@ -19,9 +19,20 @@ public class CompositePersonnelDaoTest {
 
     @Test
     public void find() {
+        CompositePersonnelDao compositePersonnelDAO=new CompositePersonnelDao();
+        Personnel P=new Personnel.Builder("Jean","Paul").telephone("02589445272").naissance(LocalDateTime.of(1997,11,18,21,05)).fonction("PDG").build();
+        CompositePersonnel compositePersonnel=new CompositePersonnel("groupe");
+        compositePersonnel.add(P);
+        compositePersonnelDAO.create(compositePersonnel);
+        CompositePersonnel p1=compositePersonnelDAO.find("Jean");
+        if (p1.equals(null)) System.out.println("reussi");
     }
 
     @Test
     public void delete() {
+        CompositePersonnelDao compositePersonnelDAO=new CompositePersonnelDao();
+        compositePersonnelDAO.delete("Jean");
+        CompositePersonnel p1=compositePersonnelDAO.find("Jean");
+        p1.equals(null);
     }
 }
